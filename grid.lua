5ff52052877239965d16f40f3340cb90822bfdff
Varagan77 <johanneswillemkotze@gmail.com>
2026-04-10 13:48:36 +0200

    tiles
    -added more tiles to play around with

--- grid.lua
+++ grid.lua
@@ -12,17 +12,55 @@ end
 
 
 function grid_draw()
-    for y=1, #map do
-		for x=1, #map[y] do
-			if map[y][x] == 1 then
-				love.graphics.rectangle("line", x * 32, y * 32, 32, 32) 
+    for y = 1, #map do
+        for x = 1, #map[y] do
+            local offset = (32 - 1) / 2
+
+            if map[y][x] == 1 then
+                love.graphics.setColor(1, 1, 1) -- white (wall)
+                love.graphics.rectangle("line", x * 32, y * 32, 32, 32)
+
             elseif map[y][x] == 0 then
-                local offset = (32 - 1) / 2
-				love.graphics.rectangle("fill", x * 32 + offset, y * 32 + offset, 1, 1) 
-                end
-			end
-		end
-	end
+                love.graphics.setColor(0.5, 0.5, 0.5) -- gray (empty)
+                love.graphics.rectangle("fill", x * 32 + offset, y * 32 + offset, 1, 1)
+
+            elseif map[y][x] == 2 then
+                love.graphics.setColor(0, 1, 0) -- green (shop)
+                love.graphics.rectangle("fill" , x * 32, y * 32, 32, 32)
+
+            elseif map[y][x] == 3 then
+                love.graphics.setColor(1, 0, 0) -- red (enemy)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+
+            elseif map[y][x] == 4 then
+                love.graphics.setColor(0, 0, 1) -- blue (portal in)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+
+            elseif map[y][x] == 5 then
+                love.graphics.setColor(1, 0.5, 0) -- orange (portal out)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+
+            elseif map[y][x] == 6 then
+                love.graphics.setColor(0.5, 1, 1) -- light blue (exit)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+
+            elseif map[y][x] == 7 then
+                love.graphics.setColor(1, 1, 0) -- yellow (gold)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+            end
+        end
+    end
+
+    love.graphics.setColor(1, 1, 1) -- reset color
+end
+    -- 1: Wall
+        -- 0: Empty Tile
+        -- 2: shop tile
+        -- 3: enemy tile
+        -- 4: portal in
+		-- 5: portal out 
+		-- 6 exit
+        -- 7: gold
 
 function testMap(x, y) --collison testing
 	if map[(player.grid_y / 32) + y][(player.grid_x / 32) + x] == 1 then
